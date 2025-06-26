80f3e05aaf849ce75c1e0d7bbee921edb7c08b06
Bastiannsp <ba.sandovalp@duocuc.cl>
2025-06-26 10:39:59 -0400

    Delete perfulandiaspa (3)/perfulandiaspa (1)/perfulandiaspa directory

--- perfulandiaspa (3)/perfulandiaspa (1)/perfulandiaspa/src/main/java/com/perfulandia/perfulandiaspa/repository/InventarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.perfulandia.perfulandiaspa.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.perfulandia.perfulandiaspa.model.InventarioModel;
-
-public interface InventarioRepository extends JpaRepository<InventarioModel, Long>{
-
-}
